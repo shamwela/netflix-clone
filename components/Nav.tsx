e1c68d5f71161a1441c9ca0aa14c563a1a9d4a2c
Sha Mwe La <shamwela2002@gmail.com>
2021-10-30 14:39:24 +0630

    Improve Nav style

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Nav = () => {
   return (
     <nav
-      className='sticky top-0 z-10 flex justify-between w-full py-4 min-h-10'
+      className='sticky top-0 z-10 flex justify-between w-full h-10 py-2 md:py-4 md:h-16'
       // style={{ backgroundColor: '#141414' }}
     >
-      <h1 className='text-red' style={{ fontSize: '1.5rem' }}>
-        NETFLIX CLONE
-      </h1>
+      <Link href='/'>
+        <a aria-label='Netflix'>
+          <h1 className='text-red' style={{ fontSize: '1.5rem' }}>
+            NETFLIX CLONE
+          </h1>
+        </a>
+      </Link>
 
       <Image
         src='/profile-icon.png'
