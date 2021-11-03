f833a309a86b2a1bb222e8f5545b2b08073adaf5
Sha Mwe La <shamwela2002@gmail.com>
2021-11-03 13:05:45 +0630

    Fix Logo and profile icon not aligning correctly

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,14 +4,12 @@ import Link from 'next/link'
 const Nav = () => {
   return (
     <nav
-      className='sticky top-0 z-10 flex justify-between w-full h-10 py-2 md:py-4 md:h-16'
+      className='sticky top-0 z-10 flex items-center justify-between w-full py-2 min-h-10 md:py-4 md:min-h-16'
       // style={{ backgroundColor: '#141414' }}
     >
       <Link href='/'>
         <a aria-label='Netflix'>
-          <h1 className='text-red' style={{ fontSize: '1.5rem' }}>
-            NETFLIX CLONE
-          </h1>
+          <h1 className='text-base text-red md:text-2xl'>NETFLIX CLONE</h1>
         </a>
       </Link>
 
