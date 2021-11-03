8c0f1fc173ecf1446b28baa4500e8c9882a0d9a8
Sha Mwe La <shamwela2002@gmail.com>
2021-11-03 11:04:00 +0630

    Imrove accessibility

--- components/Row.tsx
+++ components/Row.tsx
@@ -19,27 +19,32 @@ const Row = ({ title, fetchUrl }) => {
       <h2>{title}</h2>
 
       <section className='flex overflow-x-auto overflow-y-hidden'>
-        {movies.map(({ id, poster_path, name }) => (
-          <a
-            key={id}
-            href={
-              'https://www.youtube.com/results?search_query=netflix+trailer+' +
-              name
-            }
-            target='_blank'
-            rel='noopener'
-          >
-            {/* This div is a hack to properly size the images */}
-            <div className='w-40'>
-              <Image
-                src={'https://image.tmdb.org/t/p/original/' + poster_path}
-                alt={name}
-                width={160}
-                height={239.89}
-              />
-            </div>
-          </a>
-        ))}
+        {movies.map(({ id, poster_path, name, title }) => {
+          const movieName = name || title
+
+          return (
+            <a
+              key={id}
+              href={
+                'https://www.youtube.com/results?search_query=netflix+trailer+' +
+                movieName
+              }
+              target='_blank'
+              rel='noopener'
+              aria-label={movieName}
+            >
+              {/* This div is a hack to properly size the images */}
+              <div className='w-40'>
+                <Image
+                  src={'https://image.tmdb.org/t/p/original/' + poster_path}
+                  alt={movieName}
+                  width={160}
+                  height={239.89}
+                />
+              </div>
+            </a>
+          )
+        })}
       </section>
     </section>
   )
