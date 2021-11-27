032f9e32c08c8ca840e84ae0c53956e36d398f2f
Sha Mwe La <shamwela2002@gmail.com>
2021-11-27 16:47:08 +0630

    Add more rows

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,62 @@ export const getStaticProps = async () => {
   const trendingResponse = await fetch(
     `${baseUrl}trending/all/week?api_key=${API_KEY}&language=en-US`
   )
-  const { results: trendingMovies } = await trendingResponse.json()
+  const { results: trending } = await trendingResponse.json()
+
+  const topRatedResponse = await fetch(
+    `${baseUrl}movie/top_rated?api_key=${API_KEY}&language=en-US`
+  )
+  const { results: topRated } = await topRatedResponse.json()
+
+  const actionResponse = await fetch(
+    `${baseUrl}discover/movie?api_key=${API_KEY}&with_genres=28`
+  )
+  const { results: action } = await actionResponse.json()
+
+  const comedyResponse = await fetch(
+    `${baseUrl}discover/movie?api_key=${API_KEY}&with_genres=35`
+  )
+  const { results: comedy } = await comedyResponse.json()
+
+  const horrorResponse = await fetch(
+    `${baseUrl}discover/movie?api_key=${API_KEY}&with_genres=27`
+  )
+  const { results: horror } = await horrorResponse.json()
+
+  const romanceResponse = await fetch(
+    `${baseUrl}discover/movie?api_key=${API_KEY}&with_genres=10749`
+  )
+  const { results: romance } = await romanceResponse.json()
+
+  const documentariesResponse = await fetch(
+    `${baseUrl}discover/movie?api_key=${API_KEY}&with_genres=99`
+  )
+  const { results: documentaries } = await documentariesResponse.json()
 
   return {
-    props: { netflixOriginals, trendingMovies },
+    props: {
+      netflixOriginals,
+      trending,
+      topRated,
+      action,
+      comedy,
+      horror,
+      romance,
+      documentaries,
+    },
   }
 }
 
-const Home = ({ netflixOriginals, trendingMovies }) => {
+const Home = ({
+  netflixOriginals,
+  trending,
+  topRated,
+  action,
+  comedy,
+  horror,
+  romance,
+  documentaries,
+}) => {
   return (
     <>
       <Head>
@@ -32,7 +80,13 @@ const Home = ({ netflixOriginals, trendingMovies }) => {
       <main>
         <Banner movies={netflixOriginals} />
         <Row name='Netflix Originals' movies={netflixOriginals} />
-        <Row name='Trending' movies={trendingMovies} />
+        <Row name='Trending' movies={trending} />
+        <Row name='Top Rated' movies={topRated} />
+        <Row name='Action Movies' movies={action} />
+        <Row name='Comedy Movies' movies={comedy} />
+        <Row name='Horror Movies' movies={horror} />
+        <Row name='Romance Movies' movies={romance} />
+        <Row name='Documentaries' movies={documentaries} />
       </main>
     </>
   )
